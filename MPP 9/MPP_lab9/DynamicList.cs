d190b686a592379c66a922af4edd3f9e28f821b4
AnastasiaGolyuk <anastasiagolyuk@gmail.com>
2021-10-31 12:23:40 +0300

    initial commit

---
+++ MPP 9/MPP_lab9/DynamicList.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MPP_lab9
+{
+    public class DynamicList<T> : IEnumerable<T>
+    {
+        private T[] Items { get; set; }
+        public int Count { get; private set; }
+        public int Capacity { get; private set; }
+        private double LoadFactor { get; }
+
+        public DynamicList(int capacity, double loadFactor)
+        {
+            Items = new T[capacity];
+            Capacity = capacity;
+            LoadFactor = loadFactor;
+        }
+
+        public void Add(T elementToAdd)
+        {
+            Items[Count++] = elementToAdd;
+            if (!((double)Count / Capacity > LoadFactor))
+            {
+                return;
+            }
+            Capacity = (int)(Capacity * 1.5);
+            T[] temp = Items;
+            Items = new T[Capacity];
+            Array.Copy(temp, Items, temp.Length);
+        }
+
+        public bool Remove(T elementToRemove)
+        {
+            for (int i = 0; i < Items.Length; i++)
+            {
+                T element = Items[i];
+                if (!elementToRemove.Equals(element))
+                {
+                    continue;
+                }
+                Array.Copy(Items, i + 1,
+                    Items, i, Items.Length - i - 1);
+                Count--;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool RemoveAt(int index)
+        {
+            if (index > Items.Length || index < 0)
+            {
+                return false;
+            }
+            Array.Copy(Items, index + 1,
+                Items, index, Items.Length - index - 1);
+            Count--;
+            return true;
+        }
+
+        public void Clear()
+        {
+            Items = new T[Capacity];
+            Count = 0;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return Items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        
+        public override string ToString()
+        {
+            var s = "[";
+            for (var i = 0; i < Count; i++)
+            {
+                T t = Items[i];
+                s += t + "; ";
+            }
+
+            if (Count > 0)
+            {
+                s = s.Substring(0, s.Length - 2);
+            }
+
+            s += "]";
+            return s;
+        }
+    }
+}
---
+++ MPP 9/MPP_lab9/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MPP_lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicList<double> list = new DynamicList<double>(10, 0.75);
+            Random rnd = new Random();
+            for (int j = 0; j < 5; j++)
+            {
+                list.Add(Math.Round(rnd.NextDouble(),1));
+            }
+            list.Add(400);
+            Console.WriteLine(list.ToString());
+            Console.WriteLine("{0} {1}\n",list.Count, list.Capacity);
+            
+            for (int j = 0; j < 10; j++)
+            {
+                list.Add(Math.Round(rnd.NextDouble(),1));
+            }
+            Console.WriteLine(list.ToString());
+            Console.WriteLine("{0} {1}\n",list.Count, list.Capacity);
+
+            bool removed=list.Remove(400);
+            Console.WriteLine(list.ToString());
+            Console.WriteLine("{0} {1} {2}\n",list.Count, list.Capacity,removed);
+            
+            removed=list.Remove(10);
+            Console.WriteLine(list.ToString());
+            Console.WriteLine("{0} {1} {2}\n",list.Count, list.Capacity,removed);
+
+            removed=list.RemoveAt(3);
+            Console.WriteLine(list.ToString());
+            Console.WriteLine("{0} {1} {2}\n",list.Count, list.Capacity, removed);
+            
+            removed=list.RemoveAt(-2);
+            Console.WriteLine(list.ToString());
+            Console.WriteLine("{0} {1} {2}\n",list.Count, list.Capacity, removed);
+            
+            list.Clear();
+            Console.WriteLine(list.ToString());
+            Console.WriteLine("{0} {1}\n",list.Count, list.Capacity);
+        }
+    }
+}
